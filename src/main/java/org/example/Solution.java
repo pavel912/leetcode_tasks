81919561f5d86540a279fc63297b9b7a8a14d7d6
Pavel Lobanov <pavel.lobanov@uni-konstanz.de>
2026-02-01 17:55:27 +0100

    top 150

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,6 +1,30 @@
 package org.example;
 
 public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        //https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/?envType=study-plan-v2&envId=top-interview-150
+        ListNode dummyHead = new ListNode(0, head);
+        ListNode cur = dummyHead;
+
+        while (cur.next != null) {
+            int cnt = 0;
+            int val = cur.next.val;
+            ListNode iter = cur.next;
+            while (iter != null && iter.val == val) {
+                cnt++;
+                iter = iter.next;
+            }
+
+            if (cnt > 1) {
+                cur.next = iter;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummyHead.next;
+    }
+
     public int minimumCost(int[] nums) {
         // https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/?envType=daily-question&envId=2026-02-01
 
--- src/test/java/TestSolutions.java
+++ src/test/java/TestSolutions.java
@@ -1,9 +1,38 @@
+import org.example.ListNode;
 import org.example.Solution;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestSolutions {
+    private List<Integer> llToList(ListNode head) {
+        List<Integer> res = new ArrayList<>();
+        ListNode cur = head;
+
+        while (cur != null) {
+            res.add(cur.val);
+            cur = cur.next;
+        }
+
+        return res;
+    }
+
+    private boolean listEquals(ListNode a, ListNode b) {
+        var listA = llToList(a);
+        var listB = llToList(b);
+
+        if (listA.size() != listB.size()) return false;
+
+        for (int i = 0; i < listA.size(); i++) {
+            if (!listA.get(i).equals(listB.get(i))) return false;
+        }
+
+        return true;
+    }
     @Test
     public void testMinCost1() {
         assertEquals(6, new Solution().minimumCost(new int[] {1, 2, 3, 12}));
@@ -17,5 +46,21 @@ public class TestSolutions {
     @Test
     public void testMinCost3() {
         assertEquals(12, new Solution().minimumCost(new int[] {10, 3, 1, 1}));
+    }
+
+    @Test
+    public void testDeleteDuplicates1() {
+        ListNode input = new ListNode(1, new ListNode(1, null));
+        ListNode output = null;
+        Solution s = new Solution();
+        assertTrue(listEquals(output, s.deleteDuplicates(input)));
+    }
+
+    @Test
+    public void testDeleteDuplicates2() {
+        ListNode input = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(3, null))));
+        ListNode output = new ListNode(1, new ListNode(3, null));
+        Solution s = new Solution();
+        assertTrue(listEquals(output, s.deleteDuplicates(input)));
     }
 }

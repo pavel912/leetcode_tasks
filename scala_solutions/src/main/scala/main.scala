61fa50e77f4b4a1f67ca0afe1d69787506e9fe19
Pavel Lobanov <gta4thebest@mail.ru>
2026-03-08 17:40:28 +0100

    daily

--- scala_solutions/src/main/scala/main.scala
+++ scala_solutions/src/main/scala/main.scala
@@ -3,7 +3,7 @@ import scala.util.boundary.break
 
 @main
 def main(): Unit = {
-  println(checkOnesSegment("11111100001"))
+  println(findDifferentBinaryString(Array("111","011","001")))
 }
 
 def checkOnesSegment(s: String): Boolean = {
@@ -15,5 +15,31 @@ def checkOnesSegment(s: String): Boolean = {
       if s.charAt(i) == '1' && segment then break(false)
 
     true
+}
+
+def findDifferentBinaryString(nums: Array[String]): String = {
+  // https://leetcode.com/problems/find-unique-binary-string/?envType=daily-question&envId=2026-03-08
+
+  val n = nums.length
+
+  val ints = new Array[Int](n)
+
+  for i <- 0 until n do
+    ints(i) = Integer.parseUnsignedInt(nums(i), 2)
+
+  boundary:
+    for num <- 0 until 16 do
+      if !ints.contains(num) then break(toBinaryString(num, n))
+
+    toBinaryString(16, n)
+}
+
+def toBinaryString(num: Int, size: Int): String = {
+  val res = Integer.toBinaryString(num)
+
+  val zerosToAdd = size - res.length
+  val zeros = "0" * zerosToAdd
+
+  zeros + res
 }
 
